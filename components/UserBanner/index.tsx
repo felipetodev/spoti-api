bd92fa8ec0268869e0091637f1cb52ff1c608076
felipetodev <fe.ossandon.u@gmail.com>
2022-06-02 13:16:33 -0400

    feat: add artist hero variants

--- components/UserBanner/index.tsx
+++ components/UserBanner/index.tsx
@@ -2,16 +2,25 @@ import React from "react";
 import { Button, Image, Stack, Text } from "@chakra-ui/react";
 import { useUser } from "../../user/hooks";
 import { OptionsSongIcon } from "../SearchTrackResults";
+import { VerifiedIcon } from "../Icons";
 
-const UserBanner: React.FC<any> = ({ isPlaylist, playlistData }) => {
+const UserBanner: React.FC<any> = ({
+  isPlaylist,
+  playlistData,
+  artistData,
+}) => {
   const {
-    color,
     username,
     userImage,
     totalPublicPlaylistsCount,
     followingCount,
     followersCount,
   } = useUser();
+
+  const hasHeaderImage = artistData?.visuals?.headerImage?.sources[0]?.url;
+  const hasHeaderColor =
+    artistData?.visuals?.avatarImage?.extractedColors?.colorRaw?.hex;
+
   return (
     <>
       <Stack
@@ -19,7 +28,9 @@ const UserBanner: React.FC<any> = ({ isPlaylist, playlistData }) => {
         maxHeight={500}
         minHeight={340}
         justifyContent="flex-end"
-        backgroundColor="#421121"
+        backgroundImage={hasHeaderImage}
+        backgroundSize="cover"
+        backgroundColor={hasHeaderColor || "#421121"}
         marginTop="0px !important"
       >
         <Stack
@@ -30,82 +41,104 @@ const UserBanner: React.FC<any> = ({ isPlaylist, playlistData }) => {
           w="full"
           background="linear-gradient(transparent 0,rgba(0,0,0,.5) 100%), rgba(0,0,0,.01)"
         >
-          <Image
-            h={232}
-            w={232}
-            objectFit="cover"
-            borderRadius={isPlaylist ? "" : 9999}
-            src={
-              isPlaylist && playlistData
-                ? playlistData?.images[1].url
-                : userImage
-            }
-            alt="user"
-          />
+          {!hasHeaderImage && (
+            <Image
+              h={232}
+              w={232}
+              objectFit="cover"
+              borderRadius={isPlaylist ? "" : 9999}
+              src={
+                isPlaylist && playlistData
+                  ? playlistData?.images[0]?.url
+                  : artistData?.visuals?.avatarImage?.sources[0]?.url ||
+                    userImage
+              }
+              alt="user-avatar"
+            />
+          )}
           <Stack pl={4}>
-            <Text as="h2" fontSize="sm">
-              {isPlaylist ? "PUBLIC PLAYLIST" : "PROFILE"}
-            </Text>
+            <Stack direction="row" alignItems="center">
+              {artistData?.profile?.verified && (
+                <VerifiedIcon fill="#3d91f4" mr={2} />
+              )}
+              <Text as="h2" fontSize="sm">
+                {isPlaylist
+                  ? "PUBLIC PLAYLIST"
+                  : (artistData?.profile?.verified && "Verified Artist") ||
+                    "PROFILE"}
+              </Text>
+            </Stack>
             <Text as="h1" fontWeight={700} fontSize="96px">
-              {isPlaylist && playlistData ? playlistData.name : username}
+              {isPlaylist && playlistData
+                ? playlistData.name
+                : artistData?.profile?.name || username}
             </Text>
             <Stack direction="row">
-              {isPlaylist && playlistData.owner?.display_name && (
+              {artistData?.stats ? (
                 <Stack direction="row">
-                  <Image
-                    h={5}
-                    w={5}
-                    mr={1}
-                    draggable={false}
-                    objectFit="cover"
-                    borderRadius={9999}
-                    src={userImage}
-                    alt="user"
-                  />
+                  <Text pr={1}>{artistData.stats.monthlyListeners}</Text>
+                  <Text>monthly listeners</Text>
+                </Stack>
+              ) : (
+                <>
+                  {isPlaylist && playlistData.owner?.display_name && (
+                    <Stack direction="row">
+                      <Image
+                        h={5}
+                        w={5}
+                        mr={1}
+                        draggable={false}
+                        objectFit="cover"
+                        borderRadius={9999}
+                        src={userImage}
+                        alt="user"
+                      />
+                      <Text
+                        fontWeight={700}
+                        fontSize="sm"
+                        _after={{
+                          content: '"•"',
+                          margin: "0px 6px",
+                          fontWeight: 700,
+                        }}
+                      >
+                        {playlistData.owner.display_name}
+                      </Text>
+                    </Stack>
+                  )}
+                  <Text fontSize="sm">
+                    {isPlaylist && playlistData ? (
+                      <>{playlistData.followers.total} Likes</>
+                    ) : (
+                      <>{totalPublicPlaylistsCount} Public Playlists</>
+                    )}
+                  </Text>
                   <Text
-                    fontWeight={700}
                     fontSize="sm"
-                    _after={{
+                    _before={{
                       content: '"•"',
                       margin: "0px 6px",
                       fontWeight: 700,
                     }}
                   >
-                    {playlistData.owner.display_name}
+                    {isPlaylist && playlistData ? (
+                      <>{playlistData.tracks.total} songs</>
+                    ) : (
+                      <>{followersCount} Followers</>
+                    )}
                   </Text>
-                </Stack>
+                  <Text
+                    fontSize="sm"
+                    _before={{
+                      content: '"•"',
+                      margin: "0px 6px",
+                      fontWeight: 700,
+                    }}
+                  >
+                    {followingCount} Following
+                  </Text>
+                </>
               )}
-              <Text fontSize="sm">
-                {isPlaylist && playlistData ? (
-                  <>{playlistData.followers.total} Likes</>
-                ) : (
-                  <>{totalPublicPlaylistsCount} Public Playlists</>
-                )}
-              </Text>
-              <Text
-                fontSize="sm"
-                _before={{
-                  content: '"•"',
-                  margin: "0px 6px",
-                  fontWeight: 700,
-                }}
-              >
-                {isPlaylist && playlistData ? (
-                  <>{playlistData.tracks.total} songs</>
-                ) : (
-                  <>{followersCount} Followers</>
-                )}
-              </Text>
-              <Text
-                fontSize="sm"
-                _before={{
-                  content: '"•"',
-                  margin: "0px 6px",
-                  fontWeight: 700,
-                }}
-              >
-                {followingCount} Following
-              </Text>
             </Stack>
           </Stack>
         </Stack>
