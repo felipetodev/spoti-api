d7b8ebbec3b00c20b8b77ffec926464863c78a4e
felipetodev <fe.ossandon.u@gmail.com>
2022-06-02 13:19:05 -0400

    feat: use mock spotify info

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,13 +1,63 @@
 import React from "react";
-import { Stack } from "@chakra-ui/react";
+import { Image, Stack, Text } from "@chakra-ui/react";
+import chartsMock from "../../charts/chartsMock.json"
 
-const SearchPage: React.FC<any> = () => {
+const SearchPage: React.FC<any> = ({ data }) => {
   return (
     <Stack pt={20} px={6}>
-      <h1>Tus géneros más escuchados</h1>
-      <h1>{"/search"}</h1>
+      <Stack pb={10}>
+        <Text fontSize={24} fontWeight={700} as="h1">
+          Browse All
+        </Text>
+      </Stack>
+      <Stack
+        display="grid"
+        gridTemplateColumns={{
+          sm: "repeat(3,minmax(0,1fr))",
+          lg: "repeat(4,minmax(0,1fr))",
+          xl: "repeat(5,minmax(0,1fr))",
+        }}
+        gridTemplateRows="1fr"
+        gridAutoRows="auto"
+        gridGap={6}
+        margin="0px !important"
+        minWidth={600}
+        pb={160}
+      >
+        {data?.map((chart) => (
+          <Stack
+            key={chart?.name}
+            position="relative"
+            borderRadius="md"
+            overflow="hidden"
+          >
+            <Image
+              objectFit="cover"
+              src={chart?.images[0]?.url}
+              alt={chart?.name}
+            />
+            <Text
+              position="absolute"
+              fontSize="lg"
+              fontWeight={700}
+              p={2}
+              top={0}
+            >
+              {chart?.name}
+            </Text>
+          </Stack>
+        ))}
+      </Stack>
     </Stack>
-  )
+  );
 };
 
 export default SearchPage;
+
+export async function getStaticProps() {
+  return {
+    props: {
+      data: chartsMock,
+    },
+  };
+}
