1848b708ee05f471ed0e0ad01eb5feb2dd740998
felipetodev <fe.ossandon.u@gmail.com>
2022-06-07 18:03:02 -0400

    refact: add filter func

--- artists/api.ts
+++ artists/api.ts
@@ -1,5 +1,9 @@
 import { options } from "../utils";
 
+const getTop5 = (data: any) => {
+  return data.filter((_: any, idx: any) => idx < 5);
+};
+
 export default {
   getArtistOverview: async (artistId: any) => {
     if (!artistId) return {};
@@ -11,16 +15,18 @@ export default {
       const { data } = await response.json();
       const relatedContent = data.artist?.relatedContent;
       const artist = data.artist;
-      return { 
-        artist, 
-        discography: artist.discography.topTracks.items.filter((_: any, idx: any) => idx < 5), 
-        profile: artist.profile, 
-        featuring: relatedContent?.featuring?.items.filter((_: any, idx: any) => idx < 5), 
-        appearsOn: relatedContent?.appearsOn?.items.filter((_: any, idx: any) => idx < 5), 
-        relatedArtists: relatedContent?.relatedArtists?.items.filter((_: any, idx: any) => idx < 5), 
-        discoveredOn: relatedContent?.discoveredOn?.items.filter((_: any, idx: any) => idx < 5), 
-        visuals: artist?.visuals?.gallery?.items[0]?.sources[0]?.url ?? artist?.visuals?.avatarImage?.sources[0]?.url
-      }
+      return {
+        artist,
+        discography: getTop5(artist.discography.topTracks.items),
+        profile: artist.profile,
+        featuring: getTop5(relatedContent?.featuring?.items),
+        appearsOn: getTop5(relatedContent?.appearsOn?.items),
+        relatedArtists: getTop5(relatedContent?.relatedArtists?.items),
+        discoveredOn: getTop5(relatedContent?.discoveredOn?.items),
+        visuals:
+          artist?.visuals?.gallery?.items[0]?.sources[0]?.url ??
+          artist?.visuals?.avatarImage?.sources[0]?.url,
+      };
     } catch (e) {
       console.error(e);
       return {};
